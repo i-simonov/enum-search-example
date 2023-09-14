b5119f04ea0bf951967c7d645f02dacd94637857
i_simonov <simonov88.ivan@gmail.com>
2023-09-14 17:05:50 -0300

    by review

--- src/test/java/com/example/enumsearchexample/ControllerIntTest.java
+++ src/test/java/com/example/enumsearchexample/ControllerIntTest.java
@@ -1,6 +1,8 @@
 package com.example.enumsearchexample;
 
 
+import com.example.enumsearchexample.service.Answers;
+import com.example.enumsearchexample.controller.Controller;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,11 +22,11 @@ public class ControllerIntTest {
     MockMvc mockMvc;
 
     @ParameterizedTest
-    @ValueSource(strings = { "hi", "bye", "how are you","blah-blah-blah" })
+    @ValueSource(strings = {"hi", "bye", "how are you", "blah-blah-blah"})
     public void getAnswerTest(String param) throws Exception {
 
-        mockMvc.perform(get("http://localhost:8080/api/v1/enum-search/"+param))
+        mockMvc.perform(get("http://localhost:8080/api/v1/enum-search/" + param))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString(AnswersService.Answer(param).toString())));
+                .andExpect(content().string(containsString(Answers.findAnswer(param).toString())));
     }
 }
